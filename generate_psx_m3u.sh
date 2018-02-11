4fad9ad4387250294cf298e4aece627d02dff2eb
danyboy666 <dany.ferron@outlook.com>
2018-02-11 02:36:00 -0500

    Update generate_psx_m3u.sh

--- generate_psx_m3u.sh
+++ generate_psx_m3u.sh
@@ -12,7 +12,7 @@
 
 for i in *.cue
 do
-        title=$(echo "$i" | sed s'/.cue//g;s/ (Disc..)//g;s/ Disc..//g;s/ (.*//g')
+        title=$(echo "$i" | sed s'/.cue//g;s/ (Disc..)//g;s/ Disc..*$//g;s/ (.*//g')
 
         echo "$i" >> "$title".m3u
 done
